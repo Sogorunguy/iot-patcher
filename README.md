f41179b367adb316e2c8341a6a27515fa886c9ea
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 <ytd_ytd_ytd@mail.ru>
2022-06-10 20:10:06 +0300

    Minor fixes

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # iot-patcher
 
-TODO:                                               <br/>
-1)Create check reqs function                        <br/>
-2)Fix replace function                              <br/>
-3)Use binwalk api instead of binwalk os package     <br/>
-4)Use zipfile lib insead of zip os package          <br/>
-5)Add vulnerability scanner                         <br/>    
+TODO:<br/>
+1)Fix replace function<br/>
+2)Use zipfile lib insead of zip os package<br/>
+3)Use binwalk api instead of binwalk os package<br/>
+4)Create check reqs function<br/>
+5)Add vulnerability scanner<br/>    
+6)
--- patcher.py
+++ patcher.py
@@ -1,46 +1,26 @@
-#Libs
+######
+#Libs#
+######
+#For interate with os
 import os
+#For regular expressions
 import re
+#For ASCII drawing
+import pyfiglet
+#
 import readline, glob
-import mmap
 import crypt
-from jmespath import search
 import getpass
 from pyparsing import Regex
 from zipfile import ZipFile
-import binwalk
+
+#import binwalk
 #import apt
+#import mmap
 
 
 #Welcome text
-print("Welcome to IOT-patcher by https://github.com/TrueDru\n")
-#print("IOT-patcher requirements check:")
-#Installation check by array function
-#def package_check():
-# cache = apt.Cache()
-# cache.open()
-# packages = ['python3', 'binwalk', 'python3-apt']
-# for i in range(0, len(packages)):
-#  try:
-#   cache[packages[i]].is_installed
-#   print(packages[i], " ✅")
-#  except:
-#   print(packages[i], " ❌")
-#package_check()
-
-#Replace root hash function
-def replace_string_file(path):
- password = crypt.crypt(getpass.getpass('Please, specify new root password: '))
- file_r = open(path, 'r', encoding = 'utf-8')
- content = file_r.read()
- file_r.close()
- file_w = open(path, 'w', encoding = 'utf-8')
- regex_root = r'^root:*'
- content = re.sub(regex_root, 'root:'+password+':0:0:root:/:/bin/sh', content)
- file_w.write(content)
- file_w.close()
- print('Password for root user in '+path+' succesfully updated!\n')
- 
+print(str(pyfiglet.figlet_format("iot - patcher")+str("      Developed by https://github.com/TrueDru\n")))
 
 #Autocomplete function
 def complete(text, state):
@@ -49,6 +29,21 @@ readline.set_completer_delims(' \t\n;')
 readline.parse_and_bind("tab: complete")
 readline.set_completer(complete)
 
+#Replace root hash function
+def replace_string_file(path):
+ password = crypt.crypt(getpass.getpass('Please, specify new root password: '))
+ file=open(path, 'r+', encoding = 'utf-8')
+ start = 'root:'
+ end = ':'
+ for num, line in enumerate(f,1): 
+
+ #regex_root = r'^root:*'
+ #content = re.sub(regex_root, 'root:'+password+':0:0:root:/:/bin/sh', content)
+ #file.write(content)
+ file.close()
+ print('Password for root user in '+path+' succesfully updated!\n')
+ 
+
 #Binwalk dialog
 binpath = input('Please enter path to *.bin file you want to decompile\nPath: ') #Path to binary file
 binname = os.path.basename(binpath)                                              #Extract filename from binpath   
@@ -56,8 +51,8 @@ bindir = binpath.replace(binname,'')                                            
 outpath = input('Please enter path to output directory\nPath: ')
 if outpath == "":
   outpath = bindir
-  print("Output of binwalk wiil be in "+bindir+" directory.\n")
-  extraction = str("binwalk --signature --term -e '"+binpath+"' --log "+outpath+"binwalk.log --directory "+outpath+" -t")
+  print("Output of binwalk wiil be in the current directory.\n")
+  extraction = str("binwalk --signature --term -e '"+binpath+"' --log "+outpath+"binwalk.log -t")
 else:
   extraction = str("binwalk --signature --term -e '"+binpath+"' --log "+outpath+"binwalk.log --directory "+outpath+" -t")
 os.system(extraction)
@@ -67,13 +62,6 @@ workdir = str(bindir+'_'+binname+'.extracted/squashfs-root')
 os.chdir(workdir)
 print('\nBinwalk finished it''s work, firmware checking process is started. ')   
 
-#Passwd check
-if os.path.exists("etc/passwd") == True:
- print("Passwd file exist, start scanning")
- replace_string_file('etc/passwd')
-else:
- print("Passwd doesn't exist.\n")
-
 #Shadow check
 if os.path.exists("etc/shadow") == True:
  print("Shadow file exist, start scanning")
@@ -81,17 +69,31 @@ if os.path.exists("etc/shadow") == True:
 else:
  print("Shadow doesn't exist.\n")
 
+#Passwd check
+if os.path.exists("etc/passwd") == True:
+ print("Passwd file exist, start scanning")
+ replace_string_file('etc/passwd')
+else:
+ print("Passwd doesn't exist.\n")
+
+
+
+
+
+
+
+
+
+
+
 #Kill telnet
 if os.path.exists("etc/inetd.conf") == True:
  print("Checking inetd.conf for possible vulnerabilities")
- file_r = open('etc/inetd.conf', 'r', encoding = 'utf-8')
- content = file_r.read()
- file_r.close()
- file_w = open('etc/inetd.conf', 'w', encoding = 'utf-8')
+ file = open('etc/inetd.conf', 'r+', encoding = 'utf-8')
  content = re.sub(r'^telnet', '#telnet  stream  tcp     nowait  root    /usr/sbin/telnetd telnetd', content)
- file_w.write(content)
+ file.write(content)
  print('Succesfully disabled telnet!\n')
- file_w.close()
+ file.close()
  
 #Create new bin file
 print("Generating new binary file")
